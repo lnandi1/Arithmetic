4794b798c417c226f65309fdcd2e115f3f0fb227
lnandi1 <40141332+lnandi1@users.noreply.github.com>
2018-10-13 22:30:52 +0100

    Add files via upload

---
+++ Python_Arithmetic_12OCT2018/Chocolate_If.py
@@ -0,0 +1,5 @@
+age = 12
+if age > 9:
+     print("You are entitled to 2 chocolates")
+else:
+     print("Sorry, you are too young for chocolates!")
---
+++ Python_Arithmetic_12OCT2018/Question.py
@@ -0,0 +1,6 @@
+# Begin
+age = 12
+if age < 18:
+     print("You are too young to vote")
+else:
+     print("You can vote")
